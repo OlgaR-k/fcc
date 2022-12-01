13883509b056a0268ec8ab1381d9afa7527804cb
Olga Ramanchyk <volha.ramanchyk@gmail.com>
2022-12-01 23:20:19 +0300

    feat task

--- js.js
+++ js.js
@@ -1,22 +1,24 @@
-
-const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
-
-function golfScore(par, strokes) {
-    if (strokes == 1) {
-        return names[0];
-    } else if (par - 2 >= strokes) {
-        return names[1];
-    } else if (par - 1 == strokes) {
-        return names[2];
-    } else if (par == strokes) {
-        return names[3];
-    } else if (par + 1 == strokes) {
-        return names[4];
-    } else if (par + 2 == strokes) {
-        return names[5];
-    } else if (par + 3 <= strokes) {
-        return names[6];
-    }
-}
-
-console.log(golfScore(5, 4));
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  switch (val) {
+    case 1:
+      answer = 'alpha';
+      break;
+    case 2:
+      answer = 'beta';
+      break;
+    case 3:
+      answer = 'gamma';
+      break;
+    case 4:
+      answer = 'delta';
+      break; 
+  }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  console.log(caseInSwitch(1));
